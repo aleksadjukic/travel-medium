621ede9bcba19cafd4c3719e7812ec76d1dde41e
aleksadjukic <aleksadjukic11@gmail.com>
2021-08-26 04:50:27 +0200

    Tailwind CDN configuration

--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,9 @@
 import Route from "@ioc:Adonis/Core/Route";
 
 Route.get("/", async ({ view }) => {
-  return view.render("welcome");
+  return view.render("homepage");
+});
+
+Route.get("/about-us", async ({ view }) => {
+  return view.render("about");
 });
