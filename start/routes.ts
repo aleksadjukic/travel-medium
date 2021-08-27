746c485be49cd041e5d06ebdbd9e25e7199fb02a
aleksadjukic <aleksadjukic11@gmail.com>
2021-08-27 09:25:47 +0200

    Added login and register modal forms

--- start/routes.ts
+++ start/routes.ts
@@ -1,9 +1,13 @@
 import Route from "@ioc:Adonis/Core/Route";
 
 Route.get("/", async ({ view }) => {
-  return view.render("homepage");
+  return view.render("home");
 });
 
 Route.get("/about-us", async ({ view }) => {
   return view.render("about");
+});
+
+Route.post("/register", async ({ request }) => {
+  console.log(request.body());
 });
